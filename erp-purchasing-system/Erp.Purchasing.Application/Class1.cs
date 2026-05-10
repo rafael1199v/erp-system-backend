40bb9474ded3755df30a627c598e6427b0ed76a7
Rafael Vargas <123646379+rafael1199v@users.noreply.github.com>
2026-05-10 09:48:15 -0400

    feat: purchasing module created

---
+++ erp-purchasing-system/Erp.Purchasing.Application/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace Erp.Purchasing.Application;
+
+public class Class1
+{
+}
---
+++ erp-purchasing-system/Purchasing.API/Program.cs
@@ -0,0 +1,43 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+const string allowSpecificOrigins = "AllowSpecificOrigins";
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowSpecificOrigins,
+        policy =>
+        {
+            policy
+                .AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+            
+        });
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => 
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Open API V1");
+    });
+}
+
+//app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
